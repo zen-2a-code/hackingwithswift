d858b9ac0f93ca110c00bb1dfd90019c31359125
Stoyan Hristov <stoioxristov@gmail.com>
2026-01-07 23:26:02 +0200

    quick test for repo

---
+++ QuickTest/QuickTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickTest
+//
+//  Created by Stoyan Hristov on 7.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickTest/QuickTest/QuickTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickTestApp.swift
+//  QuickTest
+//
+//  Created by Stoyan Hristov on 7.01.26.
+//
+
+import SwiftUI
+
+@main
+struct QuickTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
