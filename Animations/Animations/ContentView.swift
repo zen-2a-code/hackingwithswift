f283ad1161be89154e02f86eb648076166a99daf
Stoyan Hristov <stoioxristov@gmail.com>
2026-01-15 00:25:21 +0200

    adding a custom transation using ViewModifer and extension of AnyTransaction

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,33 +7,44 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let rotatingDegress: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(rotatingDegress), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(rotatingDegress: -90, anchor: .topLeading), identity: CornerRotateModifier(rotatingDegress: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
-    @State private var scale = 1.0
+    @State private var isShowRed = false
+
     var body: some View {
-        Button {
-        
-        } label: {
-            Text("Tap me")
-                .padding(50)
-                .background(.red)
-                .clipShape(.circle)
-                .foregroundStyle(.white)
-                .fontWeight(.semibold)
-        }
-        .overlay {
-            Circle()
-                .stroke(.red)
-                .scaleEffect(scale)
-                .opacity(2 - scale)
-                .animation(
-                    .easeInOut(duration: 1)
-                    .repeatForever(autoreverses: false),
-                    value: scale
-                )
-        }
-        .onAppear {
-                scale = 2.0
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+
+            if isShowRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowRed.toggle()
+            }
+        }
     }
 }
 
