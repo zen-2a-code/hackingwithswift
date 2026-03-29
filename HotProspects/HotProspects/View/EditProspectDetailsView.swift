b0331d69a4c8945699dc410539d9ffdad231cc21
Stoyan Hristov <stoioxristov@gmail.com>
2026-03-29 14:00:42 +0300

    Day 85 challange 1 completed.

---
+++ HotProspects/HotProspects/View/EditProspectDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProspectDetailsView.swift
+//  HotProspects
+//
+//  Created by Stoyan Hristov on 29.03.26.
+//
+
+import SwiftUI
+
+struct EditProspectDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProspectDetailsView()
+}
